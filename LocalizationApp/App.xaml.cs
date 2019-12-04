83c53292ce8df76c962d53800caa09453a55ee36
Vlad <Vlad@DESKTOP-HJ9OVGE>
2019-12-04 20:49:39 +0200

    Initilize project

---
+++ LocalizationApp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LocalizationApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            using(FileStream fs=new FileStream("language.ini", FileMode.Open, FileAccess.Read))
+            {
+                using(StreamReader sr=new StreamReader(fs))
+                {
+                    string language = sr.ReadLine();
+                    if (!string.IsNullOrEmpty(language))
+                    {
+                        Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+                    }
+                   
+                }
+            }
+        }
+    }
+}
---
+++ LocalizationApp/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LocalizationApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUkraine_Click(object sender, RoutedEventArgs e)
+        {
+            using(FileStream fs=new FileStream("language.ini", FileMode.Create, FileAccess.Write))
+            {
+                using(StreamWriter sw=new StreamWriter(fs))
+                {
+                    sw.WriteLine("uk");
+                }
+            }
+            this.Restart();
+        }
+
+        private void btnPoland_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("language.ini", FileMode.Create, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine("pl-PL");
+                }
+            }
+            this.Restart();
+        }
+
+        private void Restart()
+        {
+            System.Diagnostics.Process.Start(
+                Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
